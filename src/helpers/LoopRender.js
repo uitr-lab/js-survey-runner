e6602d6b331424b58789bb4c4afc218366618e1e
nickolanack <nickblackwell82@gmail.com>
2024-09-17 21:30:03 -0700

    auto fmt accept options

--- src/helpers/LoopRender.js
+++ src/helpers/LoopRender.js
@@ -1,60 +1,65 @@
 export class LoopRender {
 
 	constructor(page) {
-		this._page=page
+		this._page = page
 		// this._addButtons=[];
 	}
 
-	renderFieldValueCount(fieldName, defaultRenderFn, container){
+	renderFieldValueCount(fieldName, defaultRenderFn, container, options) {
 
-		var wrap=container.appendChild(new Element('div', {
-			"class":"loop-render"
+		var wrap = container.appendChild(new Element('div', {
+			"class": "loop-render"
 		}));
 
-		var listItems=[];
-		var _lastNum=-1
-		var renderLoop=() => {
+		var listItems = [];
+		var _lastNum = -1
+		var renderLoop = () => {
 
 			var num;
 
-			if(parseInt(fieldName)+""===fieldName){
-				num=parseInt(fieldName);
-			}else{
-				num=parseInt(this._page.getFormData()[fieldName]);
+			if (parseInt(fieldName) + "" === fieldName) {
+				num = parseInt(fieldName);
+			} else {
+				num = parseInt(this._page.getFormData()[fieldName]);
 			}
-		   
-		 
 
-		   if(num===_lastNum||isNaN(num)){
-		      return;
-		   }
-		   _lastNum=num;
-		   var i=0;
-		   while(i<num){
 
-		     if(i>=listItems.length){
-		        var vehicleEl=wrap.appendChild(new Element('div', {
-		        	"class":"loop-item loop-item-"+i
-		        }));
-		        listItems.push(vehicleEl);
-		        this._page.withVariables({"loopIndex":i}, ()=>{
-		           defaultRenderFn(vehicleEl);
-		        });
-		     }else{
-		        listItems[i].style.cssText='';
-		     }
-		     i++;
-		   }
-		   while(i<listItems.length){
-		      listItems[i].style.cssText='display:none;';
-		      i++;
-		   }
-		   
+
+			if (num === _lastNum || isNaN(num)) {
+				return;
+			}
+			_lastNum = num;
+			var i = 0;
+
+			if (options&&options.startAt) {
+				i = parseInt(options.startAt);
+			}
+
+			while (i < num) {
+
+				if (i >= listItems.length) {
+					var vehicleEl = wrap.appendChild(new Element('div', {
+						"class": "loop-item loop-item-" + i
+					}));
+					listItems[i]=vehicleEl;
+					this._page.withVariables({ "loopIndex": i }, () => {
+						return defaultRenderFn(vehicleEl);
+					});
+				} else {
+					listItems[i].style.cssText = '';
+				}
+				i++;
+			}
+			while (i < listItems.length) {
+				listItems[i].style.cssText = 'display:none;';
+				i++;
+			}
+
 		};
 
 		//setTimeout(()=>{
-			//delay first render, so that caller has time to configure
-			renderLoop();
+		//delay first render, so that caller has time to configure
+		renderLoop();
 		//}, 20);
 		this._page.on('update', renderLoop);
 
@@ -62,7 +67,7 @@ export class LoopRender {
 		return this;
 
 	}
-	
+
 	// addButton(elOpt, onClick){
 
 	// 	this._addButtons.push((listEl)=>{
@@ -73,5 +78,5 @@ export class LoopRender {
 	// 	})
 
 	// }
-	
+
 }
